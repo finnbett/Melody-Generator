8dd24d9c6c9651cc3864ba471bfee5876eb0b759
Finn Bettjeman <fluxr3@gmail.com>
2021-10-08 14:20:30 +1300

    made all keys play random melody

--- script.js
+++ script.js
@@ -1,66 +1,243 @@
 const a = document.getElementById("a")
     let aAudio = new Audio();
     aAudio.src = "notes/A.mp3"
+        const notesA = ["a","b","db","d","e","gb","ab"]
+        const randomNoteA = notesA[Math.floor(Math.random() * notesA.length)]
+        const randomNoteA1 = notesA[Math.floor(Math.random() * notesA.length)]
+        const randomNoteA2 = notesA[Math.floor(Math.random() * notesA.length)]
+        console.log ((randomNoteA))
+        var randomNotePlayA = randomNoteA + "Audio.play();"
+        var randomNotePlayA1 = randomNoteA1 + "Audio.play();"
+        var randomNotePlayA2 = randomNoteA2 + "Audio.play();"
+        console.log(randomNotePlayA)
+
+const ab = document.getElementById("ab")
+    let abAudio = new Audio();
+    abAudio.src = "notes/Ab.mp3"
+        const notesAb = ["ab","bb","c","db","eb","f","g"]
+        const randomNoteAb = notesAb[Math.floor(Math.random() * notesAb.length)]
+        const randomNoteAb1 = notesAb[Math.floor(Math.random() * notesAb.length)]
+        const randomNoteAb2 = notesAb[Math.floor(Math.random() * notesAb.length)]
+        console.log ((randomNoteAb))
+        var randomNotePlayAb = randomNoteAb + "Audio.play();"
+        var randomNotePlayAb1 = randomNoteAb1 + "Audio.play();"
+        var randomNotePlayAb2 = randomNoteAb2 + "Audio.play();"
+        console.log(randomNotePlayAb)
+
 const b = document.getElementById("b")
     let bAudio = new Audio();
     bAudio.src = "notes/B.mp3"
+        const notesB = ["b","db","eb","e","gb","ab","bb"]
+        const randomNoteB = notesB[Math.floor(Math.random() * notesB.length)]
+        const randomNoteB1 = notesB[Math.floor(Math.random() * notesB.length)]
+        const randomNoteB2 = notesB[Math.floor(Math.random() * notesB.length)]
+        console.log ((randomNoteB))
+        var randomNotePlayB = randomNoteB + "Audio.play();"
+        var randomNotePlayB1 = randomNoteB1 + "Audio.play();"
+        var randomNotePlayB2 = randomNoteB2 + "Audio.play();"
+        console.log(randomNotePlayB)
+
+const bb = document.getElementById("bb")
+    let bbAudio = new Audio();
+    bbAudio.src = "notes/Bb.mp3"
+    const notesBb = ["bb","c","d","eb","f","g","a"]
+        const randomNoteBb = notesBb[Math.floor(Math.random() * notesBb.length)]
+        const randomNoteBb1 = notesBb[Math.floor(Math.random() * notesBb.length)]
+        const randomNoteBb2 = notesBb[Math.floor(Math.random() * notesBb.length)]
+        console.log ((randomNoteBb))
+        var randomNotePlayBb = randomNoteBb + "Audio.play();"
+        var randomNotePlayBb1 = randomNoteBb1 + "Audio.play();"
+        var randomNotePlayBb2 = randomNoteBb2 + "Audio.play();"
+
 const c = document.getElementById("c")
     let cAudio = new Audio();
     cAudio.src = "notes/C.mp3"
-        const notesC = "cdefgab"
+        const notesC = ["c","d","e","f","g","a","b"]
         const randomNoteC = notesC[Math.floor(Math.random() * notesC.length)]
         const randomNoteC1 = notesC[Math.floor(Math.random() * notesC.length)]
         const randomNoteC2 = notesC[Math.floor(Math.random() * notesC.length)]
         console.log ((randomNoteC))
-        var randomNotePlay = randomNoteC + "Audio.play();"
-        var randomNotePlay1 = randomNoteC1 + "Audio.play();"
-        var randomNotePlay2 = randomNoteC2 + "Audio.play();"
-        console.log(randomNotePlay)
+        var randomNotePlayC = randomNoteC + "Audio.play();"
+        var randomNotePlayC1 = randomNoteC1 + "Audio.play();"
+        var randomNotePlayC2 = randomNoteC2 + "Audio.play();"
+        console.log(randomNotePlayC)
+
 const d = document.getElementById("d")
     let dAudio = new Audio();
     dAudio.src = "notes/D.mp3"
+        const notesD = ["d","e","gb","g","a","b","db"]
+        const randomNoteD = notesD[Math.floor(Math.random() * notesD.length)]
+        const randomNoteD1 = notesD[Math.floor(Math.random() * notesD.length)]
+        const randomNoteD2 = notesD[Math.floor(Math.random() * notesD.length)]
+        console.log ((randomNoteD))
+        var randomNotePlayD = randomNoteD + "Audio.play();"
+        var randomNotePlayD1 = randomNoteD1 + "Audio.play();"
+        var randomNotePlayD2 = randomNoteD2 + "Audio.play();"
+        console.log(randomNotePlayD)
+
+const db = document.getElementById("db")
+    let dbAudio = new Audio();
+    dbAudio.src = "notes/db.mp3"
+        const notesDb = ["db","eb","f","gb","ab","bb","c","db"]
+        const randomNoteDb = notesDb[Math.floor(Math.random() * notesDb.length)]
+        const randomNoteDb1 = notesDb[Math.floor(Math.random() * notesDb.length)]
+        const randomNoteDb2 = notesDb[Math.floor(Math.random() * notesDb.length)]
+        console.log ((randomNoteDb))
+        var randomNotePlayDb = randomNoteDb + "Audio.play();"
+        var randomNotePlayDb1 = randomNoteDb1 + "Audio.play();"
+        var randomNotePlayDb2 = randomNoteDb2 + "Audio.play();"
+        console.log(randomNotePlayDb)
+
 const e = document.getElementById("e")
     let eAudio = new Audio();
     eAudio.src = "notes/E.mp3"
+    const notesE = ["e","gb","ab","a","b","db","eb"]
+        const randomNoteE = notesE[Math.floor(Math.random() * notesE.length)]
+        const randomNoteE1 = notesE[Math.floor(Math.random() * notesE.length)]
+        const randomNoteE2 = notesE[Math.floor(Math.random() * notesE.length)]
+        console.log ((randomNoteE))
+        var randomNotePlayE = randomNoteE + "Audio.play();"
+        var randomNotePlayE1 = randomNoteE1 + "Audio.play();"
+        var randomNotePlayE2 = randomNoteE2 + "Audio.play();"
+        console.log(randomNotePlayE)
+
+const eb = document.getElementById("eb")
+    let ebAudio = new Audio();
+    ebAudio.src = "notes/Eb.mp3"
+    const notesEb = ["eb","f","g","ab","bb","c","d"]
+    const randomNoteEb = notesEb[Math.floor(Math.random() * notesEb.length)]
+    const randomNoteEb1 = notesEb[Math.floor(Math.random() * notesEb.length)]
+    const randomNoteEb2 = notesEb[Math.floor(Math.random() * notesEb.length)]
+    console.log ((randomNoteEb))
+    var randomNotePlayEb = randomNoteEb + "Audio.play();"
+    var randomNotePlayEb1 = randomNoteEb1 + "Audio.play();"
+    var randomNotePlayEb2 = randomNoteEb2 + "Audio.play();"
+    console.log(randomNotePlayEb)
+
 const f = document.getElementById("f")
     let fAudio = new Audio();
     fAudio.src = "notes/F.mp3"
+    const notesF = ["f","g","a","bb","c","d","e"]
+    const randomNoteF = notesF[Math.floor(Math.random() * notesF.length)]
+    const randomNoteF1 = notesF[Math.floor(Math.random() * notesF.length)]
+    const randomNoteF2 = notesF[Math.floor(Math.random() * notesF.length)]
+    console.log ((randomNoteF))
+    var randomNotePlayF = randomNoteF + "Audio.play();"
+    var randomNotePlayF1 = randomNoteF1 + "Audio.play();"
+    var randomNotePlayF2 = randomNoteF2 + "Audio.play();"
+    console.log(randomNotePlayF)
 const g = document.getElementById("g")
     let gAudio = new Audio();
     gAudio.src = "notes/G.mp3"
+    const notesG = ["g","a","b","c","d","e","gb"]
+    const randomNoteG = notesG[Math.floor(Math.random() * notesG.length)]
+    const randomNoteG1 = notesG[Math.floor(Math.random() * notesG.length)]
+    const randomNoteG2 = notesG[Math.floor(Math.random() * notesG.length)]
+    console.log ((randomNoteG))
+    var randomNotePlayG = randomNoteG + "Audio.play();"
+    var randomNotePlayG1 = randomNoteG1 + "Audio.play();"
+    var randomNotePlayG2 = randomNoteG2 + "Audio.play();"
+    console.log(randomNotePlayG)
+
+const gb = document.getElementById("gb")
+    let gbAudio = new Audio();
+    gbAudio.src = "notes/gb.mp3"
+    const notesGb = ["gb","ab","bb","b","db","eb","f"]
+    const randomNoteGb = notesGb[Math.floor(Math.random() * notesGb.length)]
+    const randomNoteGb1 = notesGb[Math.floor(Math.random() * notesGb.length)]
+    const randomNoteGb2 = notesGb[Math.floor(Math.random() * notesGb.length)]
+    console.log ((randomNoteGb))
+    var randomNotePlayGb = randomNoteGb + "Audio.play();"
+    var randomNotePlayGb1 = randomNoteG1 + "Audio.play();"
+    var randomNotePlayGb2 = randomNoteGb2 + "Audio.play();"
+    console.log(randomNotePlayGb)
     
     
     
 a.addEventListener("click", function(){
     aAudio.play();
+    setTimeout( () => {eval(randomNotePlayA)},600);
+    setTimeout( () => {eval(randomNotePlayA1)},600 + 600);
+    setTimeout( () => {eval(randomNotePlayA2)},600 + 600 + 600);
+    
+});
+ab.addEventListener("click", function(){
+    abAudio.play();
+    setTimeout( () => {eval(randomNotePlayAb)},600);
+    setTimeout( () => {eval(randomNotePlayAb1)},600 + 600);
+    setTimeout( () => {eval(randomNotePlayAb2)},600 + 600 + 600);
     
 });
 b.addEventListener("click", function(){
     bAudio.play();
+    setTimeout( () => {eval(randomNotePlayB)},600);
+    setTimeout( () => {eval(randomNotePlayB1)},600 + 600);
+    setTimeout( () => {eval(randomNotePlayB2)},600 + 600 + 600);
+    
+});
+bb.addEventListener("click", function(){
+    bbAudio.play();
+    setTimeout( () => {eval(randomNotePlayBb)},600);
+    setTimeout( () => {eval(randomNotePlayBb1)},600 + 600);
+    setTimeout( () => {eval(randomNotePlayBb2)},600 + 600 + 600);
     
 });
 c.addEventListener("click", function(){
     cAudio.play();
-    setTimeout( () => {eval(randomNotePlay)},600);
-    setTimeout( () => {eval(randomNotePlay1)},600 + 600);
-    setTimeout( () => {eval(randomNotePlay2)},600 + 600 + 600);
+    setTimeout( () => {eval(randomNotePlayC)},600);
+    setTimeout( () => {eval(randomNotePlayC1)},600 + 600);
+    setTimeout( () => {eval(randomNotePlayC2)},600 + 600 + 600);
     
     
 });
 d.addEventListener("click", function(){
     dAudio.play();
+    setTimeout( () => {eval(randomNotePlayD)},600);
+    setTimeout( () => {eval(randomNotePlayD1)},600 + 600);
+    setTimeout( () => {eval(randomNotePlayD2)},600 + 600 + 600);
+    
+});
+db.addEventListener("click", function(){
+    dbAudio.play();
+    setTimeout( () => {eval(randomNotePlayDb)},600);
+    setTimeout( () => {eval(randomNotePlayDb1)},600 + 600);
+    setTimeout( () => {eval(randomNotePlayDb2)},600 + 600 + 600);
     
 });
 e.addEventListener("click", function(){
     eAudio.play();
+    setTimeout( () => {eval(randomNotePlayE)},600);
+    setTimeout( () => {eval(randomNotePlayE1)},600 + 600);
+    setTimeout( () => {eval(randomNotePlayE2)},600 + 600 + 600);
+    
+});
+eb.addEventListener("click", function(){
+    ebAudio.play();
+    setTimeout( () => {eval(randomNotePlayEb)},600);
+    setTimeout( () => {eval(randomNotePlayEb1)},600 + 600);
+    setTimeout( () => {eval(randomNotePlayEb2)},600 + 600 + 600);
     
 });
 f.addEventListener("click", function(){
     fAudio.play();
+    setTimeout( () => {eval(randomNotePlayF)},600);
+    setTimeout( () => {eval(randomNotePlayF1)},600 + 600);
+    setTimeout( () => {eval(randomNotePlayF2)},600 + 600 + 600);
+    
     
 });
 g.addEventListener("click", function(){
     gAudio.play();
+    setTimeout( () => {eval(randomNotePlayG)},600);
+    setTimeout( () => {eval(randomNotePlayG1)},600 + 600);
+    setTimeout( () => {eval(randomNotePlayG2)},600 + 600 + 600);
+    
+});
+gb.addEventListener("click", function(){
+    gbAudio.play();
+    setTimeout( () => {eval(randomNotePlayGb)},600);
+    setTimeout( () => {eval(randomNotePlayGb1)},600 + 600);
+    setTimeout( () => {eval(randomNotePlayGb2)},600 + 600 + 600);
     
 });
 
